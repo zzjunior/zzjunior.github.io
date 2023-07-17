fa434e4532a4c335de77b59d6b101a69f38b8ea3
zzjunior <santos.junior.706@ufrn.edu.br>
2023-07-17 02:26:58 -0300

    adicão de animações, e menu de navegacao mobile

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ function ativaEfeito(){
 }
 
 const ativaMenu = document.querySelector('.fa-bars');
-
+const navMenu = document.querySelector('header .navegacao-primaria')
 ativaMenu.addEventListener('click', ()=>{
     ativaMenu.classList.toggle('fa-x')
+    navMenu.classList.toggle('ativado')
 })
