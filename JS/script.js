83118e0d43d6ed6c34e6945f127390cff6b79c75
zzjunior <santos.junior.706@ufrn.edu.br>
2024-04-26 16:59:48 -0300

    @zzjunior

--- JS/script.js
+++ JS/script.js
@@ -1,32 +1,57 @@
-//efeito de letras digitando
-const titulo = document.querySelector('.digitando');
-const digitando = `Olá! Bem vindo ao meu Portfólio`;
-const interval = 70;
+const textoElemento = document.getElementById('texto');
+const texto = `Olá! Bem vindo ao meu Portfólio`;
+const intervaloDigitacao = 70;
+let ponteiroVisible = true;
 
-
- function ativaEfeito(titulo, digitando, interval){
-
-    titulo.innerHTML = "";
+function ativaEfeito(digitando, interval) {
     const charArray = digitando.split("");
     for (let i = 0; i < charArray.length; i++) {
-      setTimeout(() => {
-         titulo.innerHTML += charArray[i];
-         
-      }, i * interval);
+        setTimeout(() => {
+            textoElemento.textContent += charArray[i];
+            if (i === charArray.length - 1) {
+                setTimeout(() => {
+                    apagaTexto(digitando, interval);
+                }, 5000);
+            }
+        }, i * interval);
     }
 }
 
-function startRepeatingEffect(titulo, digitando, interval, repeatInterval) {
-   function run() {
-      ativaEfeito(titulo, digitando, interval);
-      setTimeout(() => {
-         run();
-      }, repeatInterval);
-   }
-   run();
+function apagaTexto(digitando, interval) {
+    const charArray = digitando.split("");
+    for (let i = charArray.length - 1; i >= 0; i--) {
+        setTimeout(() => {
+            textoElemento.textContent = textoElemento.textContent.slice(0, -1);
+            if (i === 0) {
+                setTimeout(() => {
+                    ativaEfeito(texto, interval);
+                }, 5000);
+            }
+        }, (charArray.length - i) * interval);
+    }
 }
 
-startRepeatingEffect(titulo, digitando, interval, 5000);
+function togglePonteiro() {
+    const ponteiro = document.querySelector('.ponteiro');
+    ponteiro.style.visibility = ponteiroVisible ? 'hidden' : 'visible';
+    ponteiroVisible = !ponteiroVisible;
+}
+
+function startRepeatingEffect(digitando, interval, repeatInterval) {
+    function run() {
+        togglePonteiro();
+        setTimeout(() => {
+            run();
+        }, 1200); // Ajuste o intervalo aqui (500 para 0,5 segundo)
+    }
+    ativaEfeito(digitando, interval);
+    setInterval(togglePonteiro, 500); // Ajuste o intervalo aqui também
+    run();
+}
+
+startRepeatingEffect(texto, intervaloDigitacao, 5000);
+
+
 
 //Alerta de desenvolvimento
 
